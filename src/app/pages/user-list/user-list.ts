e212368dcc5db525f938a4958560da621beda7a2
emmanuellsk8 <emmanuel50354@gmail.com>
2025-06-22 15:44:05 -0500

    Implement user management features with CRUD operations and UI components

--- src/app/pages/user-list/user-list.ts
+++ src/app/pages/user-list/user-list.ts
@@ -1,17 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from '../../service/user.service';
+import { deleteIcon } from '../../icons/delete-icon';
+import { editIcon } from '../../icons/edit-icon';
+import { User } from '../../interface/user.interface';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [NgFor, deleteIcon, editIcon],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css'
 })
-export class UserListComponent {
 
-constructor(private router: Router) {}
+export class UserListComponent implements OnInit {
 
-goToForm() {
-  this.router.navigateByUrl('/users/form');
+  users: User[] = [];
+  userToDelete?: User;
+
+
+  constructor(private router: Router, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe({
+      next: (data: User[]) => {
+        this.users = data;
+      },
+      error: (error) => {
+        console.error('Error fetching users:', error);
+      }
+    });
+  };
+
+  goToForm() {
+    this.router.navigateByUrl('/users/form');
+  }
+
+  closeModal() {
+    const modal = document.getElementById('modal') as HTMLDialogElement;
+    modal?.close();
+  }
+
+  openModal(user: User) {
+    this.userToDelete = user;
+    const modal = document.getElementById('modal') as HTMLDialogElement;
+    modal?.showModal();
+  }
+
+confirmDelete(event?: Event): void {
+  event?.preventDefault(); // opcional
+
+  if (!this.userToDelete?.id) return;
+
+  this.userService.deleteUser(this.userToDelete.id).subscribe({
+    next: () => {
+      this.users = this.users.filter(user => user.id !== this.userToDelete?.id);
+      this.closeModal();
+    },
+    error: (error) => {
+      console.error('Error deleting user:', error);
+    }
+  });
 }
+
+
 }
---
+++ src/app/service/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../interface/user.interface';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+    private API_URL = 'http://localhost:3000/api/users';
+
+    constructor(private http: HttpClient) { }
+
+    getUsers(): Observable<User[]> {
+        return this.http.get<User[]>(this.API_URL);
+    }
+
+    createUser(user: User): Observable<User> {
+        return this.http.post<User>(this.API_URL, user);
+    }
+
+    updateUser(id: number, user: User): Observable<User> {
+        return this.http.put<User>(`${this.API_URL}/${id}`, user);
+    }
+
+    deleteUser(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.API_URL}/${id}`);
+    }
+
+}
